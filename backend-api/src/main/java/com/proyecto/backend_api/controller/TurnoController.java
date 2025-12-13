8f9205e33e8e3c55f71de4e17abab9261c3d5713
kevinmatos <kevinnmatoss05@gmail.com>
2025-12-13 20:43:56 -0300

    Terminando controller de Turno

--- backend-api/src/main/java/com/proyecto/backend_api/controller/TurnoController.java
+++ backend-api/src/main/java/com/proyecto/backend_api/controller/TurnoController.java
@@ -2,11 +2,17 @@ package com.proyecto.backend_api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.List;
 import com.proyecto.backend_api.dto.TurnoRespuestaDTO;
 import com.proyecto.backend_api.dto.TurnoSolicitudDTO;
 import com.proyecto.backend_api.services.TurnoService;
@@ -17,11 +23,43 @@ public class TurnoController {
     @Autowired
     private TurnoService turnoService;
 
+    // CREAR TURNO
     @PostMapping
     public ResponseEntity<TurnoRespuestaDTO> crearTurno(@RequestBody TurnoSolicitudDTO solicitud) {
         TurnoRespuestaDTO newTurno = turnoService.reservarTurno(solicitud);
         return ResponseEntity.ok(newTurno);
-
     }
 
+    // LISTAR TURNOS CON FILTROS
+    @GetMapping
+    public ResponseEntity<List<TurnoRespuestaDTO>> listarTurnos(
+        @RequestParam(required = false) LocalDate fecha,
+        @RequestParam(required = false) String medico
+    ) {
+        return ResponseEntity.ok(turnoService.listarTurnos(fecha, medico));
+    }
+
+    // Obtener uno (ID)
+    @GetMapping("/{id}")
+    public ResponseEntity<TurnoRespuestaDTO> obtenerTurnos(
+        @PathVariable Long id
+    ) {
+        return ResponseEntity.ok(turnoService.obtenerPorId(id));
+    }
+
+    // Editar Turno
+    @PostMapping("/{id}")
+    public ResponseEntity<TurnoRespuestaDTO> actualizarTurno(
+        @PathVariable Long id,
+        @RequestBody TurnoSolicitudDTO solicitud
+    ) {
+        return ResponseEntity.ok(turnoService.editarTurno(id, solicitud));
+    }
+
+    // Eliminar Turno
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarTurno(@PathVariable Long id) {
+        turnoService.eliminarTurno(id);
+        return ResponseEntity.noContent().build();
+    }
 }
