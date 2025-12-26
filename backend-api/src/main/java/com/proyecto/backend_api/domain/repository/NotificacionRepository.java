68461032aca990339c07306ef025c9ded988ced7
kevinmatos <kevinnmatoss05@gmail.com>
2025-12-25 22:35:58 -0300

    Programando schelduler para recordatorios automaticos, modificando repostiry para admitir nuevos metodos

--- backend-api/src/main/java/com/proyecto/backend_api/domain/repository/NotificacionRepository.java
+++ backend-api/src/main/java/com/proyecto/backend_api/domain/repository/NotificacionRepository.java
@@ -1,9 +1,14 @@
 package com.proyecto.backend_api.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.proyecto.backend_api.domain.enums.TipoNotificacion;
 import com.proyecto.backend_api.domain.model.Notificacion;
+import com.proyecto.backend_api.domain.model.Turno;
 
 public interface NotificacionRepository extends JpaRepository<Notificacion, Long> {
     
+    List<Notificacion> findByTurnoAndTipo(Turno turno, TipoNotificacion tipo);
 }
